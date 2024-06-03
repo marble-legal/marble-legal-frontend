a23eb69dd7e231c689bf96201558b4a5ca7ab9e4
Ramtin Rahmati <ramtin.rahmati79@gmail.com>
2024-06-03 13:56:17 +0330

    settings responsive

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -12,9 +12,10 @@ export default function SettingsModal({ onClose }: { onClose: () => void }) {
 
   return (
     <PopupModal
-      contentClassName="max-w-[700px] mx-auto !p-0 w-full !gap-0"
+      contentClassName="md:max-w-[700px] max-w-[96vw] mx-auto !p-0 w-full !gap-0"
       onClose={onClose}
     >
+      {/* Header */}
       <div className="flex flex-row gap-1 p-4 border-b-solid border-b-[1px] border-b-[#D7D7D7]">
         <SettingsIcon
           className="[&_path]:stroke-[#80A48B]"
@@ -23,30 +24,28 @@ export default function SettingsModal({ onClose }: { onClose: () => void }) {
         />
         <span className="text-[1rem] font-[500] leading-[150%]">Settings</span>
       </div>
-      <div className="flex flex-row">
-        <div className="flex flex-col gap-2 font-[500] text-[0.875rem] leading-[110%] text-black border-r-[#D7D7D7] border-r-solid border-r-[1px] p-3">
-          <button
-            className={clsx(
-              `px-4 py-2.5 rounded-[10px] w-auto hover:bg-black/80 text-black hover:text-white transition-all text-start`,
-              {
-                "bg-black text-white": activeTab === "personal",
-              }
-            )}
+
+      <div className="flex md:flex-row flex-col">
+        {/* Tab */}
+        <div
+          className={clsx(
+            "flex gap-2 font-[500] text-[0.875rem] leading-[110%] text-black",
+            "md:border-r-[#D7D7D7] md:border-r-solid md:border-r-[1px] md:flex-col md:p-3",
+            "border-b-[#D7D7D7] border-b-solid border-b-[1px] flex-row p-2"
+          )}
+        >
+          <TabButton
+            isActive={activeTab === "personal"}
             onClick={() => setActiveTab("personal")}
           >
             My profile
-          </button>
-          <button
-            className={clsx(
-              `px-4 py-2.5 rounded-[10px] w-max hover:bg-black/80 hover:text-white text-black transition-all text-start`,
-              {
-                "bg-black text-white": activeTab === "security",
-              }
-            )}
+          </TabButton>
+          <TabButton
+            isActive={activeTab === "security"}
             onClick={() => setActiveTab("security")}
           >
             Notification settings
-          </button>
+          </TabButton>
         </div>
 
         {/* FORM */}
@@ -58,7 +57,7 @@ export default function SettingsModal({ onClose }: { onClose: () => void }) {
 
 function Personal() {
   return (
-    <div className="p-[1.5rem] w-full flex flex-col gap-4">
+    <div className="md:p-[1.5rem] p-[1.25rem] w-full flex flex-col gap-4">
       <div className="flex flex-row justify-between items-center">
         <div className="grid gap-1.5">
           <span className="text-[0.875rem] font-[500] leading-[110%]">
@@ -97,7 +96,7 @@ function Personal() {
           change
         />
       </div>
-      <div className="flex flex-row gap-5 mt-5">
+      <div className="flex flex-row gap-5 mt-5 md:mx-0 mx-auto">
         <Button
           variant="link"
           className="!text-[0.75rem] !font-[500] leading-[110%] underline !text-[#888]"
@@ -119,7 +118,7 @@ function Security() {
   const [isToggled, setIsToggled] = useState(false);
   const handleToggle = () => setIsToggled((prev) => !prev);
   return (
-    <div className="p-[1.5rem] w-full flex flex-col gap-[2.25rem]">
+    <div className="md:p-[1.5rem] p-[1.25rem] w-full flex flex-col gap-[2.25rem]">
       <div className="flex flex-col gap-4">
         <span className="text-[0.75rem] font-[600] leading-[120%] uppercase tracking-[0.48px]">
           Notification settings
@@ -143,7 +142,7 @@ function Security() {
         <span className="text-[0.75rem] font-[600] uppercase tracking-[0.48px]">
           Current Subscription
         </span>
-        <div className="border-solid border-[1px] border-[#CBD5E1] p-4 rounded-[6px] flex flex-row justify-between w-full gap-4 items-center">
+        <div className="border-solid border-[1px] border-[#CBD5E1] p-4 rounded-[6px] flex flex-row flex-wrap justify-between w-full gap-4 items-center">
           <div className="flex flex-row gap-3 items-center">
             <div className="py-[0.375rem] px-[0.625rem] bg-[#DECAFF] rounded-[6px] h-fit text-[#883EC2] text-[0.8125rem] font-[500] leading-[120%]">
               Standard
@@ -152,7 +151,7 @@ function Security() {
               $199/month
             </span>
           </div>
-          <Button className="px-[1.5rem] py-[0.675rem] h-fit !leading-[18px]">
+          <Button className="px-[1.5rem] py-[0.675rem] h-fit !leading-[18px] md:w-auto w-full">
             Upgrade
           </Button>
         </div>
@@ -160,3 +159,20 @@ function Security() {
     </div>
   );
 }
+
+const TabButton = ({ isActive, onClick, children }) => {
+  return (
+    <button
+      className={clsx(
+        "px-4 py-2.5 rounded-lg w-auto transition-all md:flex-grow-0 flex-1 min-w-[180px] md:text-start text-center",
+        {
+          "bg-black text-white": isActive,
+          "hover:bg-black/80 text-black hover:text-white": !isActive,
+        }
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
