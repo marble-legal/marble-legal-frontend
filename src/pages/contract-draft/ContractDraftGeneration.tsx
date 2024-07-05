efd500b52e6acb6456308de4c80bcc711e3cc4c4
Amit Patel <amit@micro1.ai>
2024-07-05 20:40:40 +0530

    Fix UI

--- src/pages/contract-draft/ContractDraftGeneration.tsx
+++ src/pages/contract-draft/ContractDraftGeneration.tsx
@@ -131,8 +131,8 @@ export default function ContractDraftGeneration() {
       )}
       <MobileMenu
         renderAction={
-          <div className="flex justify-end gap-2 items-center">
-            <div className="hidden md:flex ">{renderCredit()}</div>
+          <div className="flex justify-end gap-1.5 items-center">
+            <div className="[&_span]:text-xs">{renderCredit()}</div>
             <Button className="!px-2 !py-2 h-8" onClick={handleCreateDraft}>
               <PlusIcon className="!w-4 !h-4" />
             </Button>
