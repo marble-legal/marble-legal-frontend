97071d9ee8bd5bdb83c86042df0ab47b0d6e5066
Amit Patel <amit@micro1.ai>
2024-07-26 22:31:19 +0530

    Fix word break issue

--- src/pages/contract-analysis/components/DeleteContractConfirm.tsx
+++ src/pages/contract-analysis/components/DeleteContractConfirm.tsx
@@ -40,7 +40,10 @@ export function DeleteContractConfirm({
   return (
     <PopupModal contentClassName="p-8 z-[22] w-[98%] self-center bg-white rounded-2xl flex-col justify-start items-center gap-6 flex w-full md:mx-auto md:w-[442px] max-w-[442px]">
       <div className="flex flex-col w-full">
-        <p className="text-center text-xl text-black/80 font-medium">
+        <p
+          className="text-center text-xl text-black/80 font-medium"
+          style={{ wordBreak: "break-all" }}
+        >
           Are you sure you want to delete contract "{contract.title}"?
         </p>
         <div className="flex gap-3 items-center w-full mt-8">
