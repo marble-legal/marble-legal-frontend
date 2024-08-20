6c749286d747e109e250095eded3e7b5f29b3fe7
Amit Patel <amit@micro1.ai>
2024-08-20 10:15:26 +0530

    Chage auth id

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
 export const GOOGLE_OAUTH_CLIENT_ID =
-  "92288822411-2ot4gpi373j0l90a5hd0729rlnljvhbu.apps.googleusercontent.com";
+  "203915469318-lufvdhid2k5n1iu091frpodmca3l3nl4.apps.googleusercontent.com";
